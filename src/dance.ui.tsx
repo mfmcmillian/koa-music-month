de3b21b088c9a9727b15185987480073e03bb475
matt <matthew.mcmillian@ymail.com>
2023-11-15 13:22:24 -0500

    add dance

--- src/dance.ui.tsx
+++ src/dance.ui.tsx
@@ -2,22 +2,26 @@ import ReactEcs, { Button, UiEntity } from '@dcl/sdk/react-ecs';
 import { danceSystem, triggerRandomEmote } from './danceArea';
 import { engine } from '@dcl/sdk/ecs';
 
+let danceButtonText = 'Auto Dance Off'; 
+  const buttonWidth = 120; 
 
 export function DanceUI(){
-  let danceButtonText = 'Auto Dance Off'; 
-  const buttonWidth = 100; 
+  
 
   return (
     <UiEntity
     uiTransform={{
         display: 'flex',
         positionType: 'absolute',
-        position: {top: '20%', left: '40%'},
+        position: {top: '10%', left: '92%'},
         width: 300,
         justifyContent: 'space-between',
     }
 }
+
     >
+        
+        
 <Button
                     uiTransform={{width: buttonWidth, height: 40, margin: 8}}
                     value={danceButtonText}
@@ -27,7 +31,7 @@ export function DanceUI(){
                         danceButtonText = (danceButtonText == 'Auto Dance Off') ? 'Auto Dance On' : 'Auto Dance Off'
                         if (danceButtonText == 'Auto Dance Off') {
                             engine.removeSystem(danceSystem)
-                            //danceButtonText = 'Auto Dance On'
+                            
                         } else {
                             engine.addSystem(danceSystem)
                             triggerRandomEmote()
